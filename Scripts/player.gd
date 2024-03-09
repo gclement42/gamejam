5dd218ab3aa2156993d3125c8f7a82f16f2dd3ea
MarcelCerdan <1998maxime6@gmail.com>
2024-03-09 23:21:46 +0100

    Kitchen is clean except for the textures, character's movements are good

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody3D
 #Move speed in m/s
-@export var speed = 5
+@export var speed = 3
+@export var fallAcceleration = 70
 var target_velocity = Vector3.ZERO
 
 func _physics_process(delta):
@@ -8,13 +9,13 @@ func _physics_process(delta):
 	var direction = Vector3.ZERO
 	
 	if Input.is_action_pressed("moveForward"):
-		direction.x += 1
-	if Input.is_action_pressed("moveBackward"):
 		direction.x -= 1
+	if Input.is_action_pressed("moveBackward"):
+		direction.x += 1
 	if Input.is_action_pressed("moveLeft"):
-		direction.z -= 1
-	if Input.is_action_pressed("moveRight"):
 		direction.z += 1
+	if Input.is_action_pressed("moveRight"):
+		direction.z -= 1
 		
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
