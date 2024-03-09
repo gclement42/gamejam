8bd0a019706892c5f3d08fa84df2873ccc658012
gclement42 <clement.games125@gmail.com>
2024-03-09 22:45:38 +0100

    add bathroom

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody3D
 #Move speed in m/s
 @export var speed = 5
+@export var fall_acceleration = 70
+
 var target_velocity = Vector3.ZERO
 
 func _physics_process(delta):
@@ -22,6 +24,7 @@ func _physics_process(delta):
 		
 	target_velocity.x = direction.x * speed
 	target_velocity.z = direction.z * speed
+	target_velocity.y = target_velocity.y - (fall_acceleration * delta)
 	
 	velocity = target_velocity
 	move_and_slide()
